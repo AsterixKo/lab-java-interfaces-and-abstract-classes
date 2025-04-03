e9a45d5e9dc6ab9e0d34c44af99fc161335c339c
Javier Moneo <jvr.moneo@gmail.com>
2025-04-03 15:23:11 +0200

    Implement Streaming Service

---
+++ InterfacesAndAbstractClases/src/main/java/org/jmc/mydomain/videostreaming/TvSeries.java
@@ -0,0 +1,22 @@
+package org.jmc.mydomain.videostreaming;
+
+public class TvSeries extends Video {
+    private int episodes;
+
+    public TvSeries(int episodes) {
+        setEpisodes(episodes);
+    }
+
+    public TvSeries(String title, int duration, int episodes) {
+        super(title, duration);
+        setEpisodes(episodes);
+    }
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+}
